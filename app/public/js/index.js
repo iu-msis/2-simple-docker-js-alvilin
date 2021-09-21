60bf5a74f9a47ad1297e02e742506b80b94bbee5
alvilin <89392637+alvilin@users.noreply.github.com>
2021-09-21 16:46:02 -0400

    update

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,7 +1,7 @@
 const myApp = {
     data() {
       return {
-        "person":{},
+        "person":undefined,
         "firstName": "John",
         "lastName": "Doe",
         "email": "john@gmail.com",
@@ -22,29 +22,39 @@ const myApp = {
             ]
         }
     },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date).format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            console.log("A");
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                console.log("C");
+                this.person =responseJson.results[0]
+                this.firstName = responseJson.results[0].name.first
+                this.lastName = responseJson.results[0].name.last
+                this.email = responseJson.results[0].email
+                this.gender = responseJson.results[0].gender
+                this.picture = responseJson.results[0].picture.large
+                this.street = responseJson.results[0].location.street
+                this.city = responseJson.results[0].location.city
+                this.birthDate = responseJson.results[0].dob.date
+                this.age = responseJson.results[0].dob.age
+                this.country = responseJson.results[0].location.country
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            console.log("B");
+        }
+    },
     created() {
-        console.log("A");
-        fetch('https://randomuser.me/api/')
-        .then( response => response.json() )
-        .then( (responseJson) => {
-            console.log(responseJson);
-            console.log("C");
-            this.person =responseJson.results[0]
-            this.firstName = responseJson.results[0].name.first
-            this.lastName = responseJson.results[0].name.last
-            this.email = responseJson.results[0].email
-            this.gender = responseJson.results[0].gender
-            this.picture = responseJson.results[0].picture.large
-            this.street = responseJson.results[0].location.street
-            this.city = responseJson.results[0].location.city
-            this.birthDate = responseJson.results[0].dob.date
-            this.age = responseJson.results[0].dob.age
-            this.country = responseJson.results[0].location.country
-        })
-        .catch( (err) => {
-            console.error(err);
-        })
-        console.log("B");
+        this.fetchUserData();
     } //end created
 }
   
